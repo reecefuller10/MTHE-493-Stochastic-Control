c8b0344aaf53d6f281a8d7aa70ddf0d473074d73
reecefuller10 <reece.fuller@queensu.ca>
2022-11-07 18:12:26 -0500

    Hospital network enviroment

--- app/enviroment.py
+++ app/enviroment.py
@@ -4,29 +4,37 @@ import pandas as pd
 import random
 import sys
 
+#Update with the path to utils folder on your computer
 sys.path.append('/Users/reece/Documents/MTHE493/MTHE-493-Stochastic-Control/app/utils')
-print(sys.path)
+
+#debug system path
+#print(sys.path)
+
+#Import functions from helper files
 from utils.enviroment_helper import *
 from utils.transmission_helper import *
 from utils.graph_helper import *
 
 
-#ID, nurse_capacity, num_nurses, patient_capacity, num_patients, care_ratio
-
 def main():
-    print(sys.path)
-    
-    hospital_dict = create_data_dict(5)
+
+    #create a dictionary storing objects that represent each hospital, indexed by ID
+    hospital_dict = create_data_dict(5) 
+
+    #Initialize graph structure
     G = init_graph()
+
     print_hospital_data(hospital_dict)
 
-    update_hospital_attribute(hospital_dict = hospital_dict, ID = 1, attribute = 'num_patients', new_val = 30)
+    set_hospital_attribute(hospital_dict = hospital_dict, ID = 1, attribute = 'num_patients', new_val = 30)
 
     hospital_dict = move_nurse(G, hospital_dict, 2, 3)
 
     distance = get_min_distance(G, 2, 3)
 
-    update_patients(2, hospital_dict,100,0.5,0.02)
+    get_neighbours(G,2)
+
+    drift_patients(2, hospital_dict,100,0.5,0.02)
 
     print_hospital_data(hospital_dict)
 
